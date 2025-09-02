fb7ad4f6816f0eb9360cb23f3a2abb52741d3b8d
Alex <alexiusluzes@gmail.com>
2025-09-02 09:36:03 -0300

    Update exemplo_conexao.php

--- exemplo_conexao.php
+++ exemplo_conexao.php
@@ -5,7 +5,7 @@ $user = 'seu_usuario';
 $password = 'sua_senha';
 $database = 'seu_banco';
 
-$mysqli = new mysqli($host, $user, $password, $database);
+$conn = new mysqli($host, $user, $password, $database);
 
 if ($conn->connect_error) {
     die('Erro na conexão (' . $conn->connect_errno . '): ' . $conn->connect_error);
